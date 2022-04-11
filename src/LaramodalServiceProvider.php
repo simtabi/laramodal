3df4edcd16155fbf8eb505d61f9a8e160169d5d1
Imani Manyara <imani@simtabi.com>
2022-04-11 10:16:03 -0400

    Bug fixes

--- src/LaramodalServiceProvider.php
+++ src/LaramodalServiceProvider.php
@@ -32,7 +32,7 @@ class LaramodalServiceProvider extends ServiceProvider
     public function register()
     {
         $this->loadTranslationsFrom(self::PACKAGE_PATH . "resources/lang/", $this->packageName);
-        $this->loadMigrationsFrom(self::PACKAGE_PATH.'/../database/migrations');
+        $this->loadMigrationsFrom(self::PACKAGE_PATH.'database/migrations');
         $this->loadViewsFrom(self::PACKAGE_PATH . "resources/views", $this->packageName);
         $this->mergeConfigFrom(self::PACKAGE_PATH . "config/config.php", $this->packageName);
     }
